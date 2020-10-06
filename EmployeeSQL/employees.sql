647d6152541ca537f7919716fb32226cf2daaf74
Janette Chairez <ChairezFamily@Janettes-iMac.local>
2020-10-05 21:38:44 -0500

    Update to schema

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -2,58 +2,55 @@ Drop Table if exists departments;
 Drop Table if exists employees;
 Drop Table if exists dept_emp;
 Drop Table if exists dept_manager;
+Drop Table if exists salaries;
+Drop Table if exists titles;
 
 create table departments (
-	dept_no Integer Primary Key,
-	dept_name Varchar(30) not null
+	dept_no Varchar Primary Key,
+	dept_name Varchar not null
 );
 
 Select * From departments
 
+create table titles (
+	title_id Varchar Primary Key,
+	title Varchar not null
+);
+
+Select * from titles
+
+create table salaries (
+	emp_no integer Primary Key,
+	salary Float not null
+);
+
+Select * from salaries
+
 create table employees (
-	emp_no Integer not null primary key,
-	emp_title_id VARCHAR(30) not null,
+	emp_no Integer references salaries(emp_no),
+	emp_title_id VARCHAR references titles(title_id),
 	birth_date Date not null,
-	first_name VARCHAR(30) not null,
-	last_name VARCHAR(30) not null,
-	sex VARCHAR(30) not null,
-	hire_date Date not null
+	first_name VARCHAR not null,
+	last_name VARCHAR not null,
+	sex VARCHAR not null,
+	hire_date Date not null,
+	Primary Key (emp_no, emp_title_id)
 );
 
 Select * From employees
 
 create table dept_emp (
+	dept_no Varchar references departments(dept_no),
 	emp_no integer not null,
-	dept_no Integer not null,
-	emp_id integer not null,
-	Foreign Key (emp_id) references employees(emp_no)
+	Primary Key (dept_no)
 );
 
 Select * from dept_emp
 
 create table dept_manager (
-	dept_no Integer not null,
-	emp_no integer not null,
-	emp_id integer not null,
-	Foreign Key (emp_id) references employees(emp_no)
+	dept_no Varchar not null,
+	emp_no integer references salaries(emp_no),
+	Primary Key (emp_no)
 );
 
 Select * from dept_manager
-
-create table salaries (
-	emp_no integer not null,
-	salary Float not null,
-	emp_id integer not null,
-	Foreign Key (emp_id) references employees(emp_no)
-);
-
-Select * from salaries
-
-create table titles (
-	title_id integer not null,
-	title Varchar(30) not null,
-	emp_id integer not null,
-	Foreign Key (emp_id) references employees(emp_no)
-);
-
-Select * from titles
